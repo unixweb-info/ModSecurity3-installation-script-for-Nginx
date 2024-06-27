1953715bf90ea99f406cd4601bf1e4c023c10059
Aleksei Kriachko <41658912+unixweb-info@users.noreply.github.com>
2024-06-27 16:47:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -128,16 +128,16 @@ cd "nginx-$nginx_version"
 make modules && cp objs/ngx_http_modsecurity_module.so /usr/lib64/nginx/modules/ || error_exit "Failed to make and copy ModSecurity module to nginx modules directory"
 log_message "Nginx configured with ModSecurity module"
 
-# Update nginx.conf to include ModSecurity module and rules
-sed -i '8a include /usr/share/nginx/modules/*.conf;' /etc/nginx/nginx.conf || error_exit "Failed to update nginx.conf"
-sed -i '46i\    modsecurity on;' /etc/nginx/nginx.conf || error_exit "Failed to update nginx.conf"
-sed -i '47i\    modsecurity_rules_file /etc/nginx/modsecurity.d/modsecurity.conf;' /etc/nginx/nginx.conf || error_exit "Failed to update nginx.conf"
-log_message "Nginx.conf updated with ModSecurity module and rules"
-
 # Create directory for nginx modules and create modsecurity3.conf file
 if [ ! -d "/etc/nginx/modules-enabled" ]; then
   mkdir /etc/nginx/modules-enabled || error_exit "Failed to create directory for nginx modules"
 fi
+
+# Update nginx.conf to include ModSecurity module and rules
+sed -i '8a include /etc/nginx/modules-enabled/*.conf;' /etc/nginx/nginx.conf || error_exit "Failed to update nginx.conf"
+sed -i '46i\    modsecurity on;' /etc/nginx/nginx.conf || error_exit "Failed to update nginx.conf"
+sed -i '47i\    modsecurity_rules_file /etc/nginx/modsecurity.d/modsecurity.conf;' /etc/nginx/nginx.conf || error_exit "Failed to update nginx.conf"
+log_message "Nginx.conf updated with ModSecurity module and rules"
 
 echo 'load_module "/usr/lib64/nginx/modules/ngx_http_modsecurity_module.so";' > /etc/nginx/modules-enabled/modsecurity3.conf || error_exit "Failed to create modsecurity3.conf file"
 log_message "Directory for nginx modules and modsecurity3.conf created"
